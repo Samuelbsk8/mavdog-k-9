9a74bc4356c82a7e3c483a5bd5a537863bb99cb1
Samuelbsk8 <Sammybsk8@gmail.com>
2025-12-02 10:14:45 -0500

    add testimonial change

--- src/components/AddTestimonial.jsx
+++ src/components/AddTestimonial.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import EditTestimonial from "../components/EditTestimonial";
 import "../css/AddTestimonial.css";
 
 export default function AddTestimonial({ closeDialog, updateTestimonials }) {
